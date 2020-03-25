d36e47b4844e85b03311646cbe9326d8eb49519c
Pol <pol.sanchez@lifullconnect.com>
2020-03-25 12:57:09 +0100

    Add my changes into forked repo to pull request it

--- src/main/java/fizzbuzz/FizzBuzzApp.java
+++ src/main/java/fizzbuzz/FizzBuzzApp.java
@@ -1,12 +1,16 @@
 package fizzbuzz;
 
 public class FizzBuzzApp {
-    public static void main(String[] args) {
+    public static String main(String[] args) {
         FizzBuzz myFizzBuzz = new FizzBuzz();
-
-        for (String argument : args) {
-            int number = Integer.parseInt(argument);
-            System.out.println(myFizzBuzz.fizzbuzzFor(number));
+        String result = "";
+        if(args != null){
+            for (String argument : args) {
+                int number = Integer.parseInt(argument);
+                result += myFizzBuzz.fizzbuzzFor(number)+",";
+            }
+            return result.substring(0, result.length()-1);
         }
+        return "";
     }
 }
--- src/test/java/fizzbuzz/StepDefinitions.java
+++ src/test/java/fizzbuzz/StepDefinitions.java
@@ -5,7 +5,36 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.Assert.*;
+class FizzbuzzCheck{
 
+}
 public class StepDefinitions {
+    String result;
+    String[] arguments;
 
+    @Given("we send no params")
+    public void we_send_no_params() {
+        result = "";
+    }
+
+    @When("we call fizzbuzz app")
+    public void we_call_fizzbuzz_app() {
+        FizzBuzzApp fizzBuzzApp = new FizzBuzzApp();
+        result = fizzBuzzApp.main(arguments);
+    }
+
+    @Then("we should see nothing")
+    public void we_should_see_nothing() {
+        assertEquals("", result);
+    }
+
+    @Then("we should see {string}")
+    public void we_should_see(String string) {
+        assertEquals(string, result);
+    }
+
+    @Given("we send {string}")
+    public void we_send(String string) {
+        arguments = string.split(" ");
+    }
 }
